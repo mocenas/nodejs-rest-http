14c8e134e08ce8c8b5b6c030f05c1c447362dbcf
mocenas <mocenas@redhat.com>
2018-02-06 15:06:05 +0100

    Tried to create a test using Jest framework. It does not work

--- test/openshift-test.js
+++ test/openshift-test.js
@@ -1,40 +1,25 @@
-const test = require('tape');
 const OpenshiftTestAssistant = require('openshift-test-assistant');
 const openshiftAssistant = new OpenshiftTestAssistant();
 const path = require('path');
 const request = require('supertest');
 
-test('setup', (t) => {
-  t.plan(1);
-  openshiftAssistant.deploy({
+// it does not work
+beforeAll(async () => {
+  await openshiftAssistant.deploy({
     'projectLocation': path.join(__dirname, '/..'),
     'strictSSL': false
-  }).then(() => {
-    t.pass('Application deployed'); // indicate success
-  }).catch(reason => {
-    t.fail(reason);
   });
 });
 
-test('test openshift greeting with no query param', (t) => {
-  t.plan(1);
-  if (!openshiftAssistant.isReady()) {
-    t.skip('Application not ready, skipping the test');
-  } else {
-    request(openshiftAssistant.getRoute())
+test('test openshift greeting with no query param', async () => {
+  await expect(request(openshiftAssistant.getRoute())
       .get('/api/greeting')
       .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        t.equal(response.body.content, 'Hello, World', 'Received message should math the expected one');
-      })
-      .catch(reason => {
-        t.fail(reason);
-      });
-  }
+      .expect(200))
+    .resolves.toBe('Hello, World');
 });
 
-test('test openshift greeting with query param', (t) => {
+/*test('test openshift greeting with query param', (t) => {
   t.plan(1);
   if (!openshiftAssistant.isReady()) {
     t.skip('Application not ready, skipping the test');
@@ -50,14 +35,13 @@ test('test openshift greeting with query param', (t) => {
         t.fail(reason);
       });
   }
-});
+});*/
 
-test('teardown', (t) => {
-  t.plan(1);
+afterAll(() => {
   openshiftAssistant.undeploy()
     .then(() => {
       t.pass('Application successfully undeployed');
     }).catch(reason => {
       t.fail(reason);
-    });
+  });
 });
